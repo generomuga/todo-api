8343ceaa49d5077082577e361cff195e4a95fde0
Gene Romuga <generomuga12693@gmail.com>
2019-09-08 09:11:42 +0800

    Update host

--- db_config.py
+++ db_config.py
@@ -7,5 +7,5 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root@localhost'
 app.config['MYSQL_DATABASE_DB'] = 'todo'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 mysql.init_app(app)
